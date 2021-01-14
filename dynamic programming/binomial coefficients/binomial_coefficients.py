b2b29df326d4f1f9a552c9c770c825457ebeaac7
Ambreen System76 <ambreen2006@gmail.com>
2021-01-14 18:56:59 -0500

    Exercise: N choose K using dynamic programming

---
+++ dynamic programming/binomial coefficients/binomial_coefficients.py
@@ -0,0 +1,67 @@
+class BinomialCoefficients:
+    _table = [[0]]
+
+    @staticmethod
+    def extend_table(extend_by):
+        """Extend the table by extend_by number of rows"""
+        current_length = len(BinomialCoefficients._table)
+        BinomialCoefficients.compute_pascal_triangle(current_length, current_length + extend_by)
+
+    @staticmethod
+    def compute_pascal_triangle(rows_begin, rows_end):
+        """Compute Pascal Triangle
+        Args
+        ---
+        rows_begin: start computing from rows
+        rows_end: compute up to the rows
+        """
+        for i in range(rows_begin, rows_end+1):
+            BinomialCoefficients._table.append([])
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    BinomialCoefficients._table[i].append(1)
+                else:
+                    right_above = 0
+                    if j < len(BinomialCoefficients._table[i-1]):
+                        right_above = BinomialCoefficients._table[i-1][j]
+                    value_i_choose_k = BinomialCoefficients._table[i-1][j-1] + right_above
+                    BinomialCoefficients._table[i].append(value_i_choose_k)
+
+    @staticmethod
+    def print_table(table):
+        """Pretty prints the pascal's triangle"""
+        for i in range(len(table)):
+            for j in range(len(table[0])):
+                if table[i][j] == 0:
+                    break
+                print(f"{table[i][j]} ", end=" ")
+            print("")
+
+    @staticmethod
+    def n_choose_k(n, k) -> int:
+        """Returns the number of ways k can be chosen from n.
+        If n exceeds the length of the cached, compute, otherwise return saved value
+        """
+
+        BC = BinomialCoefficients
+
+        if k > n:
+            return 0
+
+        if n > len(BC._table):
+            BC.extend_table(n-len(BC._table))
+
+        return BC._table[n][k]
+
+    @staticmethod
+    def precompute(n):
+        """Computes or extends the pascal's triangle to n*n"""
+
+        BC = BinomialCoefficients
+        if len(BC._table) < n:
+            BC.extend_table(n-len(BC._table))
+
+
+BinomialCoefficients.precompute(10)
+print(BinomialCoefficients.n_choose_k(4, 3))
+print(BinomialCoefficients.n_choose_k(7, 6))
---
+++ dynamic programming/binomial coefficients/test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from math import comb
+from binomial_coefficients import BinomialCoefficients
+
+
+class BinomialCofficientsTest(TestCase):
+
+    def test_n_choose_k(self):
+        n, k = 4, 3
+        for n, k in [(4, 3), (6, 3), (7, 6), (10, 6)]:
+            expected = comb(n, k)
+            produced = BinomialCoefficients().n_choose_k(n, k)
+            print(f" n = {n}, k = {k}, Produced: {produced}, Expected: {expected}")
+            self.assertEqual(expected, produced)
+
+    def test_k_larger_than_n(self):
+        n, k = 4, 10
+        self.assertEqual(BinomialCoefficients().n_choose_k(n, k), 0)
